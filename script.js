ee7a421fe35007f105c2f2b2db23ae57eaac1e71
samrat-smju <samratahmed12x@gmail.com>
2025-03-10 23:46:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const preLoader = document.querySelector(".preloader");
-    const profile = document.querySelector(".profile");
 
     window.addEventListener("load", () => {
-        if (preLoader) preLoader.style.display = "none";
-        if (profile) profile.style.animation = "profilezoom 1s ease-out";
+        if (preLoader) {
+            preLoader.style.display = "none";
+        }
     });
 });
+
